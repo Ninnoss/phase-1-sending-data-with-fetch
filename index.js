a678841a7d041dc7c65a7cf18adb240e3795a590
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-03-25 22:55:55 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email,
+  };
+
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(userData),
+  };
+  return fetch('http://localhost:3000/users', config)
+    .then((res) => res.json())
+    .then((data) => document.body.append(`New user added: Name: ${data.name}, Email: ${data.email}, ID: ${data.id}`))
+    .catch((err) => document.body.append(err.message));
+}
